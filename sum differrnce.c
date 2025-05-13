9880d139ef7f660b9bedb6b72bbddb228ba767d5
Ganesh7411 <gd741185@gmail.com>
2025-05-13 16:26:14 +0530

    Create sum differrnce.c

---
+++ sum differrnce.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum,difference,product,quotiont;
+    printf("enter the value for b");
+    scanf("%d",&a);
+    printf("enter the value for b");
+    scanf("%d",&b);
+    sum = a+b;
+    difference =a-b;
+    product = a*b;
+    quotiont= a/b;
+    printf("%d\n%d\nd%\nd%d\n",sum,difference,product,quotiont);
+    return 0;
+}
