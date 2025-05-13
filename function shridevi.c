2fad249e79c29c38d97a350880963f50da409ffa
Ganesh7411 <gd741185@gmail.com>
2025-05-13 15:49:18 +0530

    Create function shridevi.c

---
+++ function shridevi.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int shridevi()
+{
+    printf("ganesh\n");
+    printf("dateof birth\n");
+    printf("age\n");
+    printf("mobilenumber");
+}
+int main()
+{
+    shridevi(5,10);
+    return 0;
+}
